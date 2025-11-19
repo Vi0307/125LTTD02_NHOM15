14ea4c2fe2a496f6ac7ba30c7b3f1337e9ce0910
H23sk1spkt <23115141122103@sv.ute.udn.vn>
2025-11-19 14:05:11 +0700

    Them logic chua trang appointment des

---
+++ app/src/main/java/com/example/quanlyoto/AppointmentDesActivity.java
@@ -0,0 +1,12 @@
+package com.example.quanlyoto;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AppointmentDesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_des); // layout màn 2
+    }
+}
