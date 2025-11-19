5e68111a581a3170b292ee491c9ed3f30c1ccdcd
H23sk1spkt <23115141122103@sv.ute.udn.vn>
2025-11-19 16:14:45 +0700

    them logic trang android_period

--- app/src/main/java/com/example/quanlyoto/AppointmentDesActivity.java
+++ app/src/main/java/com/example/quanlyoto/AppointmentDesActivity.java
@@ -1,12 +1,33 @@
 package com.example.quanlyoto;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.Button;
 
 public class AppointmentDesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_des); // layout màn 2
+        // Lấy reference nút back
+        ImageView btnBack = findViewById(R.id.btn_back);
+
+        // Set sự kiện click
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Kết thúc Activity hiện tại, quay về Activity trước đó
+                finish();
+            }
+        });
+        // Nút Tiếp chuyển sang màn 3: AppointmentPeriodActivity
+        Button btnNext = findViewById(R.id.button);
+        btnNext.setOnClickListener(v -> {
+            Intent intent = new Intent(AppointmentDesActivity.this, AppointmentPeriodActivity.class);
+            startActivity(intent);
+        });
     }
 }
