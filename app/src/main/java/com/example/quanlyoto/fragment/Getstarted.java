00fd4436b7b3e9b21119ade55930fe6825e0e70b
PhanMPhuc <phanminhphuc05@gmail.com>
2025-11-20 23:05:01 +0700

    chuyen qua thu muc fragment

--- app/src/main/java/com/example/quanlyoto/fragment/Getstarted.java
+++ app/src/main/java/com/example/quanlyoto/fragment/Getstarted.java
@@ -1,4 +1,4 @@
-package com.example.quanlyoto;
+package com.example.quanlyoto.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +10,8 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.quanlyoto.R;
 
 public class Getstarted extends Fragment {
 
