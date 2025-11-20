d97eb5ecc4909bfe61a85158f31c85ac1f18aefe
PhanMPhuc <phanminhphuc05@gmail.com>
2025-11-20 20:34:33 +0700

    chuyen trang welcome sang getstarted va nguoc lai

--- app/src/main/java/com/example/quanlyoto/Welcome.java
+++ app/src/main/java/com/example/quanlyoto/Welcome.java
@@ -1,12 +1,25 @@
 package com.example.quanlyoto;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Welcome extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome_screen);} // layout màn 2
+        setContentView(R.layout.activity_welcome_screen);
+
+        // Bắt sự kiện click toàn màn hình để chuyển sang GetStarted
+        View rootView = findViewById(android.R.id.content);
+        rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Welcome.this, Getstarted.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
