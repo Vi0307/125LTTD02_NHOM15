b984d3b2116bdb8f2d73829c3efa48865b079c79
VietHoang <hoang4410viet579@gmail.com>
2025-12-18 22:48:59 +0700

    BE/feature/PhuongThucVanChuyen: add controller

--- backend/src/main/java/com/example/backend/controller/VanChuyenController.java
+++ backend/src/main/java/com/example/backend/controller/VanChuyenController.java
@@ -1,4 +1,24 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.PhuongThucVanChuyenDTO;
+import com.example.backend.service.VanChuyenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/shipping")
 public class VanChuyenController {
+
+    @Autowired
+    private VanChuyenService service;
+
+    @GetMapping
+    public List<PhuongThucVanChuyenDTO> getAll() {
+        return service.getAll();
+    }
 }
+
