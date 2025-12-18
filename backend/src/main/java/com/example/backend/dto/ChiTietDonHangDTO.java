74765e64f450e67a537935caf4a93c927adeaa72
VietHoang <hoang4410viet579@gmail.com>
2025-12-19 01:27:16 +0700

    BE/feature/ChiTietDonHang: add ChiTietDonHang DTO

---
+++ backend/src/main/java/com/example/backend/dto/ChiTietDonHangDTO.java
@@ -0,0 +1,13 @@
+package com.example.backend.dto;
+
+import java.math.BigDecimal;
+
+public class ChiTietDonHangDTO {
+
+    private String maPhuTung;
+    private Integer soLuong;
+    private BigDecimal giaTien;
+
+    // Getter & Setter
+}
+
