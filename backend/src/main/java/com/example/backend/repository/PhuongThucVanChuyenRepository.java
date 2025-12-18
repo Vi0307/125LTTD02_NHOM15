859bb5969f668960c93d601d62e592a9c8cde6e0
VietHoang <hoang4410viet579@gmail.com>
2025-12-18 22:45:44 +0700

    BE/feature/PhuongThucVanChuyen: add interface

--- backend/src/main/java/com/example/backend/repository/PhuongThucVanChuyenRepository.java
+++ backend/src/main/java/com/example/backend/repository/PhuongThucVanChuyenRepository.java
@@ -1,7 +1,10 @@
 package com.example.backend.repository;
 
+import com.example.backend.entity.PhuongThucVanChuyenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PhuongThucVanChuyenRepository {
+public interface PhuongThucVanChuyenRepository extends JpaRepository<PhuongThucVanChuyenEntity, Integer> {
+
 }
