972011c88fa2ccf25e9bc011d122d18614db5d20
VietHoang <hoang4410viet579@gmail.com>
2025-12-18 22:46:24 +0700

    BE/feature/PhuongThucVanChuyen: add DTO

--- backend/src/main/java/com/example/backend/dto/PhuongThucVanChuyenDTO.java
+++ backend/src/main/java/com/example/backend/dto/PhuongThucVanChuyenDTO.java
@@ -1,4 +1,10 @@
 package com.example.backend.dto;
 
+import java.math.BigDecimal;
+
 public class PhuongThucVanChuyenDTO {
+    private Integer maPTVC;
+    private String tenPTVC;
+    private BigDecimal giaVanChuyen;
+    private Integer soNgayDuKien;
 }
