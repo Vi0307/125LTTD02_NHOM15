30ebdb71ef273f442e4ee66107a675b21a42b098
VietHoang <hoang4410viet579@gmail.com>
2025-12-18 23:13:43 +0700

    BE/feature/ApDungKhuyenMai: add get/set for dto & entity

--- backend/src/main/java/com/example/backend/entity/Voucher.java
+++ backend/src/main/java/com/example/backend/entity/Voucher.java
@@ -16,4 +16,49 @@ public class Voucher {
     private String loaiVoucher;
     private LocalDate hanSuDung;
     private String trangThai;
+
+    // Getter và Setter cho maVC
+    public Integer getMaVC() {
+        return maVC;
+    }
+
+    public void setMaVC(Integer maVC) {
+        this.maVC = maVC;
+    }
+
+    // Getter và Setter cho maND
+    public Integer getMaND() {
+        return maND;
+    }
+
+    public void setMaND(Integer maND) {
+        this.maND = maND;
+    }
+
+    // Getter và Setter cho loaiVoucher
+    public String getLoaiVoucher() {
+        return loaiVoucher;
+    }
+
+    public void setLoaiVoucher(String loaiVoucher) {
+        this.loaiVoucher = loaiVoucher;
+    }
+
+    // Getter và Setter cho hanSuDung
+    public LocalDate getHanSuDung() {
+        return hanSuDung;
+    }
+
+    public void setHanSuDung(LocalDate hanSuDung) {
+        this.hanSuDung = hanSuDung;
+    }
+
+    // Getter và Setter cho trangThai
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
 }
