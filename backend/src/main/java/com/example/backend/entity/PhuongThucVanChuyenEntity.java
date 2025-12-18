f533fb3ad75f72e4033f4b47ee03d31f9f344211
VietHoang <hoang4410viet579@gmail.com>
2025-12-18 22:43:49 +0700

    BE/feature/PhuongThucVanChuyen: add Entity

--- backend/src/main/java/com/example/backend/entity/PhuongThucVanChuyenEntity.java
+++ backend/src/main/java/com/example/backend/entity/PhuongThucVanChuyenEntity.java
@@ -1,4 +1,20 @@
 package com.example.backend.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "PHUONG_THUC_VAN_CHUYEN")
 public class PhuongThucVanChuyenEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer maPTVC;
+
+    private String tenPTVC;
+    private BigDecimal giaVanChuyen;
+    private Integer soNgayDuKien;
+    private String moTa;
 }
+
